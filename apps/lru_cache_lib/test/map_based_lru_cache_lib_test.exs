28847e450e8e640b7edba5d4464837efe6c4afa7
Masoud Mazarei <msd.mazarei@gmail.com>
2020-03-16 00:05:05 +0330

    [MapBasedLruCacheLibTest] - fixes #1 - test passed for MapBasedLruCacheLib

--- apps/lru_cache_lib/test/map_based_lru_cache_lib_test.exs
+++ apps/lru_cache_lib/test/map_based_lru_cache_lib_test.exs
@@ -6,6 +6,55 @@ defmodule MapBasedLruCacheLibTest do
     {:ok, lru_cache: MapBasedLruCacheLib.new_instance(5), lru_cache_module: MapBasedLruCacheLib}
   end
 
+
+  test "check lru-cache works for an item", state do
+    lrucache = state[:lru_cache]
+    lru_cache_module = state[:lru_cache_module]
+    lrucache = lrucache |> lru_cache_module.put("test_key", "test_value")
+    assert lru_cache_module.size(lrucache) == 1
+    {exists, value, _} = lrucache |> lru_cache_module.get("test_key")
+    assert value == "test_value" and exists == true
+  end
+
+  test "check lru-cache consider capacity", state do
+    lrucache = state[:lru_cache]
+    lru_cache_module = state[:lru_cache_module]
+
+    lrucache =
+      1..(lrucache |> lru_cache_module.capacity())
+      |> Enum.reduce(lrucache, fn x, lrucache ->
+        lrucache |> lru_cache_module.put(to_string(x), to_string(x))
+      end)
+
+    assert lru_cache_module.size(lrucache) == lru_cache_module.capacity(lrucache)
+
+    lrucache = lrucache |> lru_cache_module.put("11", "11")
+    assert lru_cache_module.size(lrucache) == lru_cache_module.capacity(lrucache)
+
+    # oldest item should not exists
+    {false, nil, _} = lru_cache_module.get(lrucache, "1")
+  end
+
+  test "check lru-cache remove Last recent Used Item", state do
+    lrucache = state[:lru_cache]
+    lru_cache_module = state[:lru_cache_module]
+
+    lrucache =
+      1..(lrucache |> lru_cache_module.capacity())
+      |> Enum.reduce(lrucache, fn x, lrucache ->
+        lrucache |> lru_cache_module.put(to_string(x), to_string(x))
+      end)
+
+    assert lru_cache_module.size(lrucache) == lru_cache_module.capacity(lrucache)
+    {_, _, lrucache} = lru_cache_module.get(lrucache, "1")
+    # right now by accessing element "1", the oldest item is "2"
+    # adding new item should remove "2"
+    lrucache = lrucache |> lru_cache_module.put("11", "11")
+    # oldest item should not exists
+    {false, nil, _} = lru_cache_module.get(lrucache, "2")
+    {true, "1", _} = lru_cache_module.get(lrucache, "1")
+  end
+
   test "check consider capacity", state do
     lru_cache = state[:lru_cache]
     lru_cache_module = state[:lru_cache_module]
